359f2d83430060f990bcc91922ec11e1e03a813b
Matilda Mared <matildamared@live.se>
2022-08-20 16:02:28 +0200

    restart the game if the player presses space bar

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
 test_font = pygame.font.Font("font/Pixeltype.ttf", 48)
-
+game_active = True
 text_color = (64, 64, 64)
 box_color = "#c0e8ec"
 
@@ -28,38 +28,46 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if player_rect.collidepoint(event.pos) and player_rect.bottom == 300:
-                player_gravity = -20
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE and player_rect.bottom == 300:
-                player_gravity = -20
 
-    # Background
-    screen.blit(sky_surface, (0, 0))
-    screen.blit(ground_surface, (0, 300))
+        if game_active:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if player_rect.collidepoint(event.pos) and player_rect.bottom == 300:
+                    player_gravity = -20
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and player_rect.bottom == 300:
+                    player_gravity = -20
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
+                snail_rect.left = 800
 
-    pygame.draw.rect(screen, box_color, score_rect, 8, 10)
-    pygame.draw.rect(screen, box_color, score_rect, 12, 10)
-    screen.blit(score_surface, score_rect)
+    if game_active:
+        # Background
+        screen.blit(sky_surface, (0, 0))
+        screen.blit(ground_surface, (0, 300))
 
-    # Snail
-    screen.blit(snail_surface, snail_rect)
-    snail_rect.x -= 4
-    if snail_rect.right <= 0:
-        snail_rect.x = 800
+        pygame.draw.rect(screen, box_color, score_rect, 8, 10)
+        pygame.draw.rect(screen, box_color, score_rect, 12, 10)
+        screen.blit(score_surface, score_rect)
 
-    # Player
-    player_gravity += 1
-    player_rect.y += player_gravity
-    if player_rect.bottom >= 300:
-        player_rect.bottom = 300
-    screen.blit(player_surface, player_rect)
+        # Snail
+        screen.blit(snail_surface, snail_rect)
+        snail_rect.x -= 4
+        if snail_rect.right <= 0:
+            snail_rect.x = 800
 
-    # Collision
-    if snail_rect.colliderect(player_rect):
-        pygame.quit()
-        exit()
+        # Player
+        player_gravity += 1
+        player_rect.y += player_gravity
+        if player_rect.bottom >= 300:
+            player_rect.bottom = 300
+        screen.blit(player_surface, player_rect)
+
+        # Collision
+        if snail_rect.colliderect(player_rect):
+            game_active = False
+    else:
+        screen.fill("Pink")
 
     pygame.display.update()
     clock.tick(60)
