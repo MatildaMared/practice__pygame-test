1a69492034187c629ac887f25dae079221f4662c
Matilda Mared <matildamared@live.se>
2022-08-20 16:23:14 +0200

    add score

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import pygame
 from sys import exit
 
+
+def display_score():
+    current_time = int((pygame.time.get_ticks() - start_time) / 1000)
+    score_surface = test_font.render(f"Score: {current_time}", False, text_color)
+    score_rect = score_surface.get_rect(center=(400, 50))
+    screen.blit(score_surface, score_rect)
+
+
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner")
@@ -9,12 +17,13 @@ test_font = pygame.font.Font("font/Pixeltype.ttf", 48)
 game_active = True
 text_color = (64, 64, 64)
 box_color = "#c0e8ec"
+start_time = 0
 
 sky_surface = pygame.image.load("graphics/Sky.png").convert()
 ground_surface = pygame.image.load("graphics/ground.png").convert()
 
-score_surface = test_font.render("The most awesome game!", False, text_color)
-score_rect = score_surface.get_rect(center=(400, 100))
+# score_surface = test_font.render("The most awesome game!", False, text_color)
+# score_rect = score_surface.get_rect(center=(400, 100))
 
 snail_surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
 snail_rect = snail_surface.get_rect(midbottom=(800, 300))
@@ -40,15 +49,17 @@ while True:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
                 snail_rect.left = 800
+                start_time = pygame.time.get_ticks()
 
     if game_active:
         # Background
         screen.blit(sky_surface, (0, 0))
         screen.blit(ground_surface, (0, 300))
+        display_score()
 
-        pygame.draw.rect(screen, box_color, score_rect, 8, 10)
-        pygame.draw.rect(screen, box_color, score_rect, 12, 10)
-        screen.blit(score_surface, score_rect)
+        # pygame.draw.rect(screen, box_color, score_rect, 8, 10)
+        # pygame.draw.rect(screen, box_color, score_rect, 12, 10)
+        # screen.blit(score_surface, score_rect)
 
         # Snail
         screen.blit(snail_surface, snail_rect)
